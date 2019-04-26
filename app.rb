0e644cefb792f155f8cd25bb818b040355b616c5
Alison Reilly <reillyamr@gmail.com>
2019-04-26 13:44:04 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -15,11 +15,11 @@ class App < Sinatra::Base
   
   post '/puppy' do 
     
-    name = params[:name]
-    breed = params[:breed]
-    age = params[:age]
+    @name = params[:name]
+    @breed = params[:breed]
+    @age = params[:age]
     
-    new_puppy = Puppy.new(name, breed, age) 
+    new_puppy = Puppy.new 
     
     erb :display_puppy 
   end 
