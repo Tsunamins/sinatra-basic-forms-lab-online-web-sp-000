891141607a9ea374f02487f2ea01e561eaf0688f
Alison Reilly <reillyamr@gmail.com>
2019-04-26 13:32:04 +0000

    Automatically backed up by Learn

--- models/puppy.rb
+++ models/puppy.rb
@@ -2,7 +2,7 @@ class Puppy
   
   attr_accessor :name, :breed, :months_old 
   
-  def new_puppy(name, breed, months_old)
+  def add_puppy(name, breed, months_old)
     @name = name 
     @breed = breed 
     @months_old = months_old
